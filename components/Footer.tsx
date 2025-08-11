cb3142864410798b473d275c1714997656c84e61
Ugorji E Victor <80261775+BlckTitan@users.noreply.github.com>
2025-08-11 14:05:38 +0100

    Added a footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer id='footer' className='w-full h-1/12'>Footer</footer>
+    <footer id='footer' className='w-full h-1/12 flex justify-center items-center'>
+      <p className='text-sm text-white'>&copy; Ugorji Victor Eze. All rights reserved 2025.</p>
+    </footer>
   )
 }
