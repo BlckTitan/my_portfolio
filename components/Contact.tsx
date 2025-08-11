fdf3f6c816b1982b9b0d9d0afa879134474dd639
Ugorji E Victor <80261775+BlckTitan@users.noreply.github.com>
2025-08-11 14:54:20 +0100

    created the contact section

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { FaWhatsapp } from 'react-icons/fa'
+import { MdLocalPhone } from 'react-icons/md'
+import { TfiEmail } from 'react-icons/tfi'
 
 export default function Contact() {
   return (
-    <div>
-      <h3 className='inline-block lg:hidden mb-2 text-left text-white text-lg font-semibold'>Contact</h3>
+    <div className='py-4'>
+      <h3 className='hidden lg:inline-block  mb-2 text-left text-white text-lg font-semibold'>Contact</h3>
+
+      <div className='w-full h-fit'>
+
+        <div className='contact-text'>
+          <i><MdLocalPhone /></i> 
+          <span>(+234) 8134484139</span>
+        </div>
+
+        <div className='contact-text my-4'>
+          <i><FaWhatsapp /></i> 
+          <span>(+234) 8134484139</span>
+        </div>
+
+        <div className='contact-text'>
+          <i><TfiEmail /></i> 
+          <span>ugorjivictoreze@gmail.com</span>
+        </div>
+        
+      </div>
+
     </div>
   )
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer id='footer' className='w-full h-1/12 flex justify-center items-center'>
+    <footer id='footer' className='w-full h-1/12 flex justify-center items-center py-4'>
       <p className='text-sm text-white'>&copy; Ugorji Victor Eze. All rights reserved 2025.</p>
     </footer>
   )
