ef292dcb84709f7fb4c0b435536d984fa46d6ee1
Ugorji E Victor <80261775+BlckTitan@users.noreply.github.com>
2025-08-11 14:02:05 +0100

    Created the projects section

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,4 +1,7 @@
+import Image from 'next/image'
 import React from 'react'
+import Octa from '@/public/octaCryptoApp.webp';
+import WLBT from '@/public/WLBT.webp';
 
 export default function Projects() {
 
@@ -13,20 +16,21 @@ export default function Projects() {
   ]
 
   const data = [
-  {
-    id: 1,
-    date: 'PRESENT',
-    title: 'inSpace',
-    description: 'Easybank landing page gives quick information and guides user action with a CTA. This project fully responsive.'
-  },
-  {
-    id: 2,
-    date: '2023',
-    link: '#',
-    title: 'WorldLink',
-    description: 'Easybank landing page gives quick information and guides user action with a CTA. This project fully responsive.'
-  }
-]
+    {
+      id: 1,
+      img: WLBT,
+      stack: 'Built with: HTML, CSS, JS, React, Styled-Components, SmtpJS',
+      title: 'WLBT Tour Reservation APP',
+      description: 'WLBT tour Reservation app is designed to collect basic reservation information to different tourist destinations from vacationers and vacation plans can be prebooked ahead of high-season. This app lets users choose their destination, hotel facility, airport pickup and dropoff, passenger gender, duration of stay, passenger size. This project demonstrates my understanding of react and my ability to see out a project from ideation to completion.'
+    },
+    {
+      id: 2,
+      img: Octa,
+      stack: 'Built with: HTML, CSS, MaterialUI, Material-icons, JS, React, Redux, RTKQuery, chartJS, SASS, Restful Api/rapidApi',
+      title: 'Octa cryptoApp',
+      description: 'Octa cryptoApp was built to give information on the market performance of the different cryptocurrencies as well related news articles. This app utilizes chartJS to give a visual representation of statistical data to help influence purchase decisions. This project demonstrate my ability to use react, redux and RTK Query consume Api data. It also demonstrates my ability to use material UI and other skills listed above.'
+    }
+  ]
 
   return (
     <div className='mt-16'>
@@ -48,12 +52,19 @@ export default function Projects() {
         <h3 className='inline-block lg:hidden mb-2 text-left text-white text-lg font-semibold'>Projects</h3>
         {
           data.map((item) => (
-            <div key={item.id} className='w-full h-fit mb-2 flex flex-col lg:flex-row justify-between items-start rounded-md p-4 hover:bg-white/15 hover:backdrop-blur-md'>
-              <h3 className='w-28 mr-3 mb-2 lg:mb-0'>{item.date}</h3>
+            <div key={item.id} className='w-full h-fit mb-2 flex flex-col justify-between items-start rounded-md p-4 hover:bg-white/15 hover:backdrop-blur-md'>
+              <Image
+                alt='Project image'
+                src={item.img}
+                className='w-full h-80 mb-4'
+                objectFit='cover'
+                objectPosition='center'
+              />
               <div className='w-full h-full flex flex-col items-start justify-start'>
                 <h2 className='text-white font-semibold'>{item.title}</h2>
                 <p className='mt-4'>{item.description}</p>
               </div>
+              <h6 className='w-full text-[var(--tag-foreground)] text-sm mt-2'>{item.stack}</h6>
             </div>
           ))
         }
